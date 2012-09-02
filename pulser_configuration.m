01f883593f2da4a91860c857f7eda47f2724faae
cdeister <cdeister@brown.edu>
2012-09-02 11:01:01 -0400

    Core functionality works, now with NIMEX as the backend.
    1) Included Svoboda lab NIMEX package.
    
    2) Changed the configuration file to work with NIMEX @Task class
    
    3) Quadrature encoding removed temporarily.
    
    4) Digital out should work, but I've not specified any line/port
    options that will certainly be needed.
    
    5) Still need to queue data.

--- pulser_configuration.m
+++ pulser_configuration.m
@@ -7,7 +7,7 @@
 
 %% Ni Daqs
 
-% Abandoned the toolbox. It's a great thing, but too limited. Nimex to the rescue?
+% Abandoned the DAQtoolbox. It's a great thing, but too limited. Nimex to the rescue?
 pulserSession.ni.rate = 10000;
 
 % Here we set the cards you want to use ('Dev1', 'Dev2', etc.). 
@@ -15,23 +15,44 @@ pulserSession.ni.rate = 10000;
 % Each block here needs a vector (syntax is  like cards=['Dev1' 'Dev2'] and count=[4 8]). 
 pulserSession.ni.daqToggle=0;      % 0 means no daqs, 1 means use daqs
 
-pulserSession.ni.AOut.names=['Piezo','Blue Laser','Yellow Laser','output4'];
-pulserSession.ni.AOut.devIDs=['Dev1','Dev1','Dev1','Dev1'];  % I've thought about different/better ways to do this, but the user has to make choices at some point, might as well be at the earliest point.
- 
-% Name of connected object(s) (for your own sake)
-pulserSession.ni.AIn.names=['Air','Land','Sea','input4'];
-pulserSession.ni.AOut.devIDs=['Dev1','Dev1','Dev1','Dev1']; 
-pulserSession.ni.DigOut.Names=['Fiber Launch Shutter'];
-pulserSession.ni.DigOut.devIDs=['Dev1'];  
+%% ----- Analog Out
+%
+
+pulserSession.ni.aOut.devIDs={'Dev1','Dev1','Dev1','Dev1'};  % I default to the cell array approach even for one card for maximum flexibility. 
+pulserSession.ni.aOut.names={'Piezo','Blue Laser','Yellow Laser','output4'};
+pulserSession.ni.aOut.channels={0 1 2 3};  % could be numeric arrays in the cells.
+% This is the default scheme the @Task class wants the devIDs to be in.
+% From Task Class:
+%   deviceNames: String or string cell array specifying names of device on which channel(s) should be added, e.g. 'Dev1'. If a cell array, chanIDs must also be a cell array (of equal length).
+%   chanIDs: A numeric array of channel IDs or, in the case of multiple deviceNames (a multi-device Task), a cell array of such numeric arrays
+%   chanNames: (OPTIONAL) A string or string cell array specifying names to assign to each of the channels in chanIDs (if a single string, the chanID is appended for each channel) In the case of a multi-device Task, a cell array of such strings or string cell arrays. If omitted/empty, then default DAQmx channel name is used.
+
+%% ----- Analog In
+% 
+pulserSession.ni.aIn.names={'Air','Land','Sea','input4'};
+pulserSession.ni.aIn.devIDs={'Dev1','Dev1','Dev1','Dev1'}; 
+
+%% ----- Digital Out
+%
+pulserSession.ni.digOut.Names={'Fiber Launch Shutter'};
+pulserSession.ni.digOut.devIDs={'Dev1'};  
+
+%   deviceNames: String or string cell array specifying names of device on which channel(s) should be added, e.g. 'Dev1'. If a cell array, chanIDs must also be a cell array (of equal length).
+%   chanIDs: A string identifying port and/or line IDs for this Channel, e.g. 'port0','port0/line0:1', or 'line0:15'. In the case of multiple deviceNames (a multi-device Task), a cell array of such strings
+%   chanNames: (OPTIONAL) A string or string cell array specifying names to assign to each of the channels in chanIDs (if a single string, the chanID is appended for each channel) In the case of a multi-device Task, a cell array of such strings or string cell arrays. If omitted/empty, then default DAQmx channel name is used.
+%   lineGrouping: (OPTIONAL) One of {'DAQmx_Val_ChanPerLine', 'DAQmx_Val_ChanForAllLines'}. If empty/omitted, 'DAQmx_Val_ChanForAllLines' is used. Specifies whether to group digital lines into one or more virtual channels. If you specify one or more entire ports in chanIDs, you must set lineGrouping to DAQmx_Val_ChanForAllLines.
+
+%% ----- Quadrature Encoding
+%
 
 % I haven't looked into nimex quadrature encoding, so I comment out for now.
 % pulserSession.ni.counterCount=[0];
 % pulserSession.ni.counterType=['position'];
 % pulserSession.ni.CountertNames=['Rotary1']; 
 
-%% Optical Encoders  (optical mouse chips connected to arduinos; dx and dys are streamed over serial)
+%% ----- Optical Encoders  (optical mouse chips connected to arduinos; dx and dys are streamed over serial)
+%
 
-% Configure arduino-based optical encoders if you want.
 pulserSession.opticalEncoder.encoderToggle=1;
 pulserSession.opticalEncoder.count=1;
 pulserSession.opticalEncoder.baudRates=(38400);
--- pulser_sessionConstructor.m
+++ pulser_sessionConstructor.m
@@ -3,30 +3,41 @@
 % CAD 8/28/2012
 
 
-%%  Daq Configure  (nidaqs can do analog out, analog in, and often counters. Matlab session does not allow digital outs.)
+%%  Daq Configure  (Configure DAQ 'tasks' for aOut, aIN etc.)
+
+% Only run if the the daqToggle bit is flipped. The user does this explicitly in the configuration, but it could be flipped by certain GUI states.
+%
 
 if pulserSession.ni.daqToggle
 	import dabs.ni.daqmx.*
-
-% You might want multiple analog input and output cards so we have to loop
-% the adds to span through all the cards in the configuration file. 
-    if numel(pulserSession.AOutCount > 0)
-        for i=1,numel(pulserSession.ni.AOutCount)
-            pulser_daq_session.addAnalogOutputChannel(pulserSession.ni.cards(i), 0:pulserSession.ni.AOutCount(i)-1, 'Voltage');
+  
+	% I need to count channels.     
+	%function chanObjs = createAOVoltageChan(obj,deviceNames,chanIDs,chanNames,minVal,maxVal,units,customScaleName)
+	if numel(pulserSession.aOut.devIDs > 0)
+		pulserSession.aOut.task=Task('pulserAO')  %Create a task object; needs a string identifier.
+		pulserSession.aOut.task.createAOVoltageChan(pulserSession.ni.aOut.devIDs,pulserSession.ni.aOut.channels,pulserSession.ni.aOut.names,-10,10);
+    end
+    
+    if numel(pulserSession.ni.aIn.names > 0)
+        for i=1:numel(pulserSession.ni.aIn.names)
+			pulserSession.aIn.task=Task('pulserAI')  %Create a task object; needs a string identifier.
+			pulserSession.aIn.task.createAIVoltageChan(pulserSession.ni.aIn.devIDs,pulserSession.ni.aIn.channels,pulserSession.ni.aIn.names,-10,10);
+        end
+    end
+	
+	% function chanObj = createDOChan(obj,deviceNames,chanIDs,chanNames,lineGrouping)
+    if numel(pulserSession.ni.digOut.names > 0)
+        for i=1:numel(pulserSession.ni.digOut.names)
+			pulserSession.digOut.task=Task('pulserDigOut')  %Create a task object; needs a string identifier.
+			pulserSession.digOut.task.createDOChan(pulserSession.ni.digOut.devIDs,pulserSession.ni.digOut.channels,pulserSession.ni.digOut.names);
         end
     end
     
-    if numel(pulserSession.ni.AInCount > 0)
-        for i=1,numel(pulserSession.ni.AInCount)
-            pulser_daq_session.addAnalogInputChannel(pulserSession.ni.cards(i), 0:pulserSession.ni.AInCount(i)-1, 'Voltage');
-        end
-    end
-    
-    if numel(pulserSession.ni.counterCount > 0)
-        for i=1,numel(pulserSession.ni.counterCount)
-            pulser_daq_session.addCounterInputChannel(pulserSession.ni.cards(i), 0:pulserSession.ni.counterCount(i)-1, pulserSession.ni.counterType(i));
-        end
-    end
+%    if numel(pulserSession.ni.counterCount > 0)
+%        for i=1,numel(pulserSession.ni.counterCount)
+%            pulser_daq_session.addCounterInputChannel(pulserSession.ni.cards(i), 0:pulserSession.ni.counterCount(i)-1, pulserSession.ni.counterType(i));
+%        end
+%    end
 
 else
 end
